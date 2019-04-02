3acbb2e6ca11728c34873ec84534533d53de25d2
Setup <33972291+shift093@users.noreply.github.com>
2019-04-02 19:11:12 +0800

    Update modifyGeojsonA2B.py

--- modifyGeojson/modifyGeojsonA2B.py
+++ modifyGeojson/modifyGeojsonA2B.py
@@ -3,12 +3,15 @@ file_poly = 'file_poly.geojson'
 
 import json
 with open(file_line, 'r') as f:
-    data = json.load(f)
+    data = json.load(f)#讀入'file.geojson'檔到data
 
 for feature in data['features']:
     if (feature['geometry']['type'] == 'LineString') & (len(feature['geometry']['coordinates']) >= 3):
         feature['geometry']['type'] = 'Polygon'
         feature['geometry']['coordinates'].append(feature['geometry']['coordinates'][0])
 
+#with open(configFile, 'w+') as f:
+#        f.write(json.dumps(confsDict, indent=2))
+#
 with open(file_poly, 'w+') as f:
     json.dump(data, f, indent=2)
